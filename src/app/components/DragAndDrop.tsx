df52ce68144715b6721bfc1bde4796b5807402a4
Alex Virdee <alex.virdee08@gmail.com>
2024-07-31 17:16:06 -0400

    chore: dropzone integration

---
+++ src/app/components/DragAndDrop.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { useState } from "react"
+import Dropzone from "react-dropzone"
+
+export default function DragAndDrop() {
+    const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+
+
+    const handleDrop = (acceptedFiles: any[]) => {
+        if (acceptedFiles.length === 0) {
+            console.log("no rfp files");
+            return
+        }
+
+        // Process the selected PDF files
+        console.log("Selected RFP example files", acceptedFiles);
+
+        // Call function to handle the RFP files
+        // TODO: pass to back end program for processing 
+        setUploadedFiles(acceptedFiles);
+    }
+
+    return (
+        <>
+            <Dropzone onDrop={handleDrop}>
+                {({ getRootProps, getInputProps }) => (
+                    <section className="dropZone">
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Drag & drop some files here, or click to select files</p>
+                        </div>
+                    </section>
+                )}
+            </Dropzone>
+            {/*  Display files that were dropped */}
+            {uploadedFiles.length > 0 && (
+                <div>
+                    <p>Uploaded RFP files</p>
+                    <ul>
+                        {uploadedFiles.map((file, index) => (
+                            <li key={index}>{file.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import React, { useCallback } from 'react';
+import DragAndDrop from './components/DragAndDrop';
+
 export default function Home() {
+
+
+
   return (
-    <div>
+    <div className='main-container'>
       <h1>LEGAI RFP Drag & Drop UI</h1>
+      <DragAndDrop />
     </div>
   );
 }
